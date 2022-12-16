635186454a994de045eb6fe3068913ac9d66d1e8
pabloTokuo <contatopbo@hotmail.com>
2022-12-16 09:36:33 -0300

    Referencing posts to users

--- src/main/java/com/springcourse/workshopmongo/config/Instantiation.java
+++ src/main/java/com/springcourse/workshopmongo/config/Instantiation.java
@@ -41,5 +41,9 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, sdf.parse("23/03/2022"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        // Referenciando os posts para o usuario
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
