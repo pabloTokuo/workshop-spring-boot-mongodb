ed7f709d76d76f0560b217590f9b2d609f1869d2
pabloTokuo <contatopbo@hotmail.com>
2022-12-16 11:22:14 -0300

    @Query

--- src/main/java/com/springcourse/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/springcourse/workshopmongo/repository/PostRepository.java
@@ -2,12 +2,15 @@ package com.springcourse.workshopmongo.repository;
 
 import com.springcourse.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+    @Query("{ 'title': { $regex: ?0, $options: '<i>' } }")
+    List<Post> findByTitle(String text);
 
     // Query Methods para buscar titulo com tal texto
     List<Post> findByTitleContainingIgnoreCase(String text);
